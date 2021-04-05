86fe8230ca904febb6ccd7deb3ab856c1dd34104
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-04-05 16:26:50 +0700

    test

--- api/lastlot.php
+++ api/lastlot.php
@@ -2,7 +2,7 @@
 //ยังใช้งานไม่ได้ในvercel
 error_reporting(E_ERROR | E_PARSE);
 header('Access-Control-Allow-Origin: *');
-$days=json_decode(file_get_contents("https://lottsanook.vercel.app/api/god.php"));
+$days=json_decode(file_get_contents("https://lottsanook.vercel.app/api/gdpy.php?year=".date('Y')+543));
 $get=json_decode(file_get_contents("https://lottsanook.vercel.app/api/?date=".end($days)));
 echo json_encode(array("win"=>$get[0][1],"threefirst"=>$get[1][1].",".$get[1][2],"threeend"=>$get[2][1].",".$get[2][2],"twoend"=>$get[3][1]));
 ?>
