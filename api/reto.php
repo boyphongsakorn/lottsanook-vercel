fa4bc57d4a19c75a5bdc63c5368811274082a073
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2021-04-10 23:33:56 +0700

    Create reto.php

---
+++ api/reto.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+header('Access-Control-Allow-Origin: *');
+$url = "https://lottsanook.herokuapp.com/?date=".date("dmY");
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($ch);
+curl_close($ch);
+$result=json_decode($response);
+if($result[8][100] == "xxxxxx"){
+    echo "yes";
+} else {
+    echo "no";
+}
+?>
