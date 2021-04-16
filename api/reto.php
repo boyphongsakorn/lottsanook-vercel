d899cdccc1e9995a4f29f7f9d1cd4f104bba58f3
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-04-16 10:39:38 +0700

    test

--- api/reto.php
+++ api/reto.php
@@ -2,6 +2,7 @@
 error_reporting(E_ERROR | E_PARSE);
 header('Access-Control-Allow-Origin: *');
 $url = "https://lottsanook.vercel.app/api/?date=".date("dmY");
+echo $url;
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
