2dccee8e3506fe9121117cb8fee05e09249916a8
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-06-09 21:24:14 +0700

    test

--- api/lastlot.php
+++ api/lastlot.php
@@ -5,5 +5,9 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 $days=json_decode(file_get_contents("https://lottsanook.vercel.app/api/gdpy.php?year=".date('Y')+543));
 $get=json_decode(file_get_contents("https://lottsanook.vercel.app/api/?date=".end($days)));
-echo json_encode(array("win"=>$get[0][1],"threefirst"=>$get[1][1].",".$get[1][2],"threeend"=>$get[2][1].",".$get[2][2],"twoend"=>$get[3][1]));
+if(isset($_GET['info'])){
+    echo json_encode(array("info"=>array("date"=>end($days)),"win"=>$get[0][1],"threefirst"=>$get[1][1].",".$get[1][2],"threeend"=>$get[2][1].",".$get[2][2],"twoend"=>$get[3][1]));
+}else{
+    echo json_encode(array("win"=>$get[0][1],"threefirst"=>$get[1][1].",".$get[1][2],"threeend"=>$get[2][1].",".$get[2][2],"twoend"=>$get[3][1]));
+}
 ?>
